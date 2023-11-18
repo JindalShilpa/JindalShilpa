1eecdf31a60a91e71f33a80f7df9f85089ae5454
Shilpa Jindal <shilpajindal2002@gmail.com>
2023-11-18 10:24:01 +0530

    addition added in calculator

---
+++ calculator.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    float x,y;
+    int choice;
+    printf("enter x any y");
+    scanf("%f%f",&x,&y);
+    printf("enter your choice\n1.Addition\n2.subtraction\n3.multiplicatio\n4.division\n");
+    scanf("%d",&choice);
+    switch(choice)
+    {
+        case 1: printf("addition is %f",x+y);
+    }
+}
